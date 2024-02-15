e9913620fd7afef79e9a6d489d01b80ca93d77ab
C. D. Godfriedt <60560876+crxunch@users.noreply.github.com>
2024-02-15 17:33:48 -0600

    Create project1.java

---
+++ project1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Miles driven on Monday: ");
+        int m_hours = scnr.nextInt();
+        System.out.print("Miles driven on Tuesday: ");
+        int t_hours = scnr.nextInt();
+        System.out.print("Miles driven on Wednesday: ");
+        int w_hours = scnr.nextInt();
+        System.out.print("Miles driven on Thursday: ");
+        int r_hours = scnr.nextInt();
+        System.out.print("Miles driven on Friday: ");
+        int f_hours = scnr.nextInt();
+        System.out.print("Miles driven on Saturday: ");
+        int s_hours = scnr.nextInt();
+        System.out.print("Miles driven on Sunday: ");
+        int d_hours = scnr.nextInt();
+        
+        System.out.println("Week\tMonday\tTuesay\tWednesday\tThursday\tFriday\tSaturday\tSunday\tTotal/Week");
+        for (int i = 1; i < 6; i++) {
+            System.out.println(i);
+            for (int j = 0; y < 8; j++) {
+                
+            }
+        }
+    }
+}
