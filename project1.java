2f331acd63b3056a8ded27853ed5478ba8a98c76
C. D. Godfriedt <60560876+crxunch@users.noreply.github.com>
2024-02-16 23:35:57 -0600

    Update project1.java

--- project1.java
+++ project1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         System.out.print("Miles driven on Monday: ");
@@ -18,12 +18,18 @@ class HelloWorld {
         System.out.print("Miles driven on Sunday: ");
         int d_hours = scnr.nextInt();
         
-        System.out.println("Week\tMonday\tTuesay\tWednesday\tThursday\tFriday\tSaturday\tSunday\tTotal/Week");
-        for (int i = 1; i < 6; i++) {
-            System.out.println(i);
-            for (int j = 0; y < 8; j++) {
-                
+        System.out.println("Week\tMonday\tTuesday\tWednesday\tThursday\tFriday\tSaturday\tSunday\tTotal/Week");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + "\t\t");
+            for (int j = 0; j < 7; j++) {
+                if (i == 5 && j >= 2) {
+                    System.out.print(String.format("%-8s", "0"));
+                } else {
+                    int day = (7*(i-1) + j) + 1;
+                    System.out.print(String.format("%-8s", day));
+                }
             }
+            System.out.print("\n");
         }
     }
 }
